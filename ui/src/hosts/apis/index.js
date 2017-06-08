3cfc8c2732e9b557f6e86d188eefdb72e26305de
Arne Demmers <arne.demmers@gmail.com>
2017-06-08 09:03:36 +0200

    Remove fixed telegraf database reference from hosts list queries.

--- ui/src/hosts/apis/index.js
+++ ui/src/hosts/apis/index.js
@@ -10,7 +10,7 @@ export function getCpuAndLoadForHosts(proxyLink, telegrafDB) {
       select non_negative_derivative(mean(uptime)) as deltaUptime from "system" where time > now() - 10m group by host, time(1m) fill(0);
       select mean("Percent_Processor_Time") from win_cpu where time > now() - 10m group by host;
       select mean("Processor_Queue_Length") from win_system where time > now() - 10s group by host;
-      select non_negative_derivative(mean("System_Up_Time")) as deltaUptime from "telegraf"."autogen"."win_uptime" where time > now() - 10m group by host, time(1m) fill(0);
+      select non_negative_derivative(mean("System_Up_Time")) as deltaUptime from "win_uptime" where time > now() - 10m group by host, time(1m) fill(0);
       show tag values from /win_system|system/ with key = "host"`,
     db: telegrafDB,
   }).then(resp => {
